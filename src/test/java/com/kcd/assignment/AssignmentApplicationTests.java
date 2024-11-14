6d76019a67eed9da3399cab933b0cb22eec10de3
KYJ <kyj1210zirh@gmail.com>
2024-11-15 04:43:02 +0900

    first commit

---
+++ src/test/java/com/kcd/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcd.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
