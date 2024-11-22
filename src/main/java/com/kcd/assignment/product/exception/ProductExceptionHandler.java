5e7c84b4a5a53d65e615968563f52eeb28ea3409
KYJ <kyj1210zirh@gmail.com>
2024-11-22 10:58:48 +0900

    필드 에러 생성해야함

--- src/main/java/com/kcd/assignment/product/exception/ProductExceptionHandler.java
+++ src/main/java/com/kcd/assignment/product/exception/ProductExceptionHandler.java
@@ -2,20 +2,27 @@ package com.kcd.assignment.product.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice(basePackages = "com.kcd.assignment.product.controller")
 public class ProductExceptionHandler {
     private static final Logger log = LoggerFactory.getLogger(ProductExceptionHandler.class);
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ProductErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        
+        // TODO : FieldError를 이용하여 ProductError를 생성하고 ProductErrorResponse로 변환하여 반환
+
+        return null;
+    }
+
     @ExceptionHandler(ProductException.class)
-    public ProductErrorResponse handleProductException(ProductException e) {
+    public ResponseEntity<ProductErrorResponse> handleProductException(ProductException e) {
         log.error("ProductException: {}", e.getMessage());
 
-        return ProductErrorResponse.of(e.getError());
+        return ResponseEntity.badRequest().body(ProductErrorResponse.of(e.getError()));
     }
 }
